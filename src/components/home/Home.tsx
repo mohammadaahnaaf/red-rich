5b72c1646a79e91d4c1bf903ccada2c2eddaf495
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-06-27 01:54:09 +0600

    logo

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import React from 'react'
 import { Inter } from 'next/font/google'
+import { Header } from '@redrich/shared'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -8,21 +9,22 @@ type Props = {}
 
 export const Home = (props: Props) => {
     return (
-        <main
-            className={`flex min-h-screen flex-col items-center justify-center p-24 ${inter.className}`}
-        >
-            <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-pink-200 after:via-red-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700/10 after:dark:from-pink-900 after:dark:via-[#ff0001]/40 before:lg:h-[360px]">
-                <Image
-                    className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-                    src="/next.svg"
-                    alt="Next.js Logo"
-                    width={180}
-                    height={37}
-                    priority
-                />
-            </div>
-
-      
-        </main>
+        <div>
+            <Header />
+            <main
+                className={`flex flex-col gap-5 w-full items-center justify-center p-10 ${inter.className}`}
+            >
+                <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-pink-200 after:via-red-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-red-700/10 after:dark:from-pink-900 after:dark:via-[#ff0001]/40 before:lg:h-[360px]">
+                    <Image
+                        className="relative dark:drop-shadow-[0_0_0.3rem_#ee6b6e]"
+                        src="/logo/red-rich.svg"
+                        alt="Next.js Logo"
+                        width={580}
+                        height={237}
+                        priority
+                    />
+                </div>
+            </main>
+        </div>
     )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
+import { Home } from '@redrich/home'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function MainPage() {
   return (
-    <div>Hello</div>
+    <>
+      <Head>
+        <title>RedRich | Home</title>
+      </Head>
+      <div>
+        <Home />
+      </div>
+    </>
   )
 }
