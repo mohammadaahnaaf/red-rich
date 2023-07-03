5d4a9442cbac83e344f0769b3a63a9206dcc63ab
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-07-03 17:19:25 +0600

    about page

--- src/components/others/About.tsx
+++ src/components/others/About.tsx
@@ -14,8 +14,8 @@ export const About = (props: Props) => {
     return (
         <div className='bg-gray-100'>
 
-            <div className='max-w-6xl mx-auto text-black'>
-                <h2 className='text-black text-3xl text-center py-4 font-semibold'>About RedRich</h2>
+            <div className='max-w-6xl py-8 mx-auto text-black'>
+                <h2 className='text-gray-600 text-3xl text-center py-8 font-semibold'>ABOUT REDRICH</h2>
 
                 {/* top about  */}
                 <div className='grid grid-cols-3 items-start h-full p-4 gap-4 min-h-[70vh]'>
@@ -57,28 +57,52 @@ export const About = (props: Props) => {
                         RedRich builds technologies that help people invest, discover the world and grow businesses.
                     </h3>
                 </div>
-            </div>
 
-            {/* video player */}
-            <div>
-                <div className='min-h-screen'>
+                {/* middle about */}
+                <div className='p-4'>
+                    <div className='h-[50vh] w-full rounded-2xl grid grid-cols-2 bg-gray-600'>
+                        <div className='bg-[#510D10] grid items-center rounded-l-2xl'>
+                            <div className='grid gap-4 px-10'>
+                                <h3 className='text-2xl font-semibold text-gray-100'>
+                                    Who Are We?</h3>
+                                <p className='text-lg font-semibold text-white'>
+                                    RedRich is a sister group of <span className='border-b hover:border-[red] hover:text-[red]'>Royal Ahnafia Limited.</span> RedRich offers luxarious services like Resort, Resturent, Camping, Adventure, Sports and many more.
+                                </p>
+                                <button className='w-28 p-2 text-[#510D10] rounded-full font-semibold bg-white'>Learn More</button>
+                            </div>
+                        </div>
 
-                    <div className='flex w-full bg-white shadow-md overflow-hidden mx-auto'>
-                        <div className="flex flex-col">
-                            <div className="relative">
-                                <video className="w-full" controls={true} autoPlay={true} loop={true} muted={mute}>
-                                    <source src="/videos/video1.mp4" type="video/mp4" />
-                                    Your browser does not support the video tag.
-                                </video>
-                                <button
-                                    type='button'
-                                    onClick={() => setMute(!mute)}
-                                    className='z-20 absolute w-full top-0 flex items-center h-full justify-center bg-black hover:bg-opacity-0 bg-opacity-5 text-transparent '>Mute</button>
+                        <div className='rounded-r-2xl'>
+                            <div className="h-[50vh] w-full relative">
+                                <Image
+                                    src={"/banners/banner-woman.jpg"}
+                                    alt="woman-image"
+                                    fill
+                                    className="rounded-r-2xl object-cover"
+                                />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+
+            {/* video player */}
+
+            <div className='flex w-full bg-white shadow-md overflow-hidden mx-auto'>
+                <div className="flex flex-col">
+                    <div className="relative">
+                        <video className="w-full" controls={true} autoPlay={true} loop={true} muted={mute}>
+                            <source src="/videos/video1.mp4" type="video/mp4" />
+                            Your browser does not support the video tag.
+                        </video>
+                        <button
+                            type='button'
+                            onClick={() => setMute(!mute)}
+                            className='z-20 absolute w-full top-0 flex items-center h-full justify-center bg-white hover:bg-opacity-0 bg-opacity-5 text-transparent'>Mute</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     )
 }
