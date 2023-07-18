31ae37c43cc55f331924fd87f016a816ec772e83
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-07-19 01:15:59 +0600

    all

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -6,16 +6,25 @@ type Props = {}
 export const Footer = (props: Props) => {
     let year = new Date().getFullYear()
     return (
-        <footer className='bg-white py-4 text-center'>
-            <p className='text-gray-400 font-semibold'>
-                Copyright © {year}{' '}
-                <span className="text-sm text-red-400 hover:text-[red] cursor-pointer text-center font-bold">
-                    <Link href="https://red-rich.vercel.app">
-                        RedRich™{' '}
-                    </Link>
-                </span>
-                All rights reserved.
-            </p>
+        <footer className='bg-white py-4'>
+            <div className="grid grid-cols-4 max-w-6xl w-full mx-auto">
+                <div className='col-span-1'>
+                    <p className='text-gray-400 font-semibold'>
+                        © {year}{' '}
+                        <span className="text-sm text-red-400 hover:text-[red] cursor-pointer text-center font-bold">
+                            <Link href="https://red-rich.vercel.app">
+                                RedRich™{' '}
+                            </Link>
+                        </span>
+                    </p>
+                </div>
+                <div className='col-span-3 grid md:flex gap-4 justify-between p-4 items-center'>
+                    <p>About</p>
+                    <p>Conditions</p>
+                    <p>Location</p>
+                </div>
+               
+            </div>
         </footer>
     )
 }
