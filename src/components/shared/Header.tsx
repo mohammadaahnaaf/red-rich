3c5c5197eb3742db73c695f44cdcc925e4b7d4d7
Mohammad Ahnaf Al-Rahmany <69469668+mohammadaahnaaf@users.noreply.github.com>
2023-07-20 04:39:34 +0600

    all

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,19 +1,40 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 type Props = {
     // setLink: any
 }
 
-const styles = {
-    link: 'hover:scale-125 duration-200 hover:text-black text-rose-600'
-}
+
 
 export const Header = (props: Props) => {
 
+    // const [isOpen, setIsOpen] = useState(false)
+    const [prevScrollPosition, setPrevScrollPosition] = useState(0)
+    const [visable, setVisable] = useState(true)
     const router = useRouter()
+
+    const styles = {
+        link: visable ? 'hover:scale-125 duration-200 hover:text-black text-rose-600' : 'text-white hover:text-[yellow]'
+    }
+
+    const handleScroll = () => {
+        const currentScrollPosition = window.scrollY
+
+        if (currentScrollPosition > prevScrollPosition) {
+            setVisable(false)
+        } else {
+            setVisable(true)
+        }
+        setPrevScrollPosition(currentScrollPosition)
+    }
+
+    useEffect(() => {
+        window.addEventListener("scroll", handleScroll)
+        return () => window.removeEventListener("scroll", handleScroll)
+    })
 
     function handleLink(i: string) {
         router.push(i)
@@ -24,7 +45,9 @@ export const Header = (props: Props) => {
     return (
         <div
             id='headers'
-            className='bg-white bg-opacity-40 z-40 backdrop-blur-sm'
+            className={`transition ease-linear duration-600 bg-white z-40 backdrop-blur-sm ${visable ? 'bg-opacity-40' : 'opacity-1 bg-[red]'
+                }`}
+        // className='bg-white bg-opacity-40 z-40 backdrop-blur-sm'
         >
             <header className='py-2 max-w-6xl mx-auto flex items-center justify-evenly gap-8'>
                 <div className='md:flex hidden gap-8 items-center font-semibold'>
@@ -33,16 +56,37 @@ export const Header = (props: Props) => {
                     <Link className={styles.link} href='/restaurant'>Resturent</Link>
                 </div>
                 <div>
-                    <button type='button' onClick={() => router.push('/')} className='grid hover:scale-[1.2] duration-500 text-center text-sm text-[red] font-semibold'>
-                        <Image
-                            className="relative"
-                            src="/logo/red-rich-white.svg"
-                            alt="Red Rich Logo"
-                            width={100}
-                            height={100}
-                            priority
-                        />
-                        R E D R I C H
+                    <button type='button' onClick={() => router.push('/')} className='grid hover:scale-[1.2] duration-500'>
+                        {visable ? (
+                            <>
+                                <Image
+                                    className="relative"
+                                    src="/logo/red-rich-red.svg"
+                                    alt="Red Rich Logo"
+                                    width={100}
+                                    height={100}
+                                    priority
+                                />
+                                <span className='text-center text-sm text-[red] font-semibold'>
+                                    R E D R I C H
+                                </span>
+                            </>
+                        ) : (
+                            <>
+                                <Image
+                                    className="relative"
+                                    src="/logo/red-rich-white.svg"
+                                    alt="Red Rich Logo"
+                                    width={100}
+                                    height={100}
+                                    priority
+                                />
+                                <span className='text-center text-sm text-[white] font-semibold'>
+                                    R E D R I C H
+                                </span>
+                            </>
+                        )}
+
                     </button>
                 </div>
                 <div className='md:flex hidden gap-8 items-center font-semibold'>
